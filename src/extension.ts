70e4247d00fbfe31e11f45d1925d782d7be4e6f4
Bryan Veloso <bryan@velo.so>
2024-11-27 20:49:27 -0800

    Just get something showing in the sidebar + configuration.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,53 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
+import { OBSWebSocket } from "obs-websocket-js"
 
-export function activate(context: vscode.ExtensionContext) {}
+class OBSControlViewProvider implements vscode.WebviewViewProvider {
+  private _view?: vscode.WebviewView
+  private obs: OBSWebSocket
+
+  constructor(private readonly _extensionUri: vscode.Uri) {
+    this.obs = new OBSWebSocket()
+  }
+
+  private _updateView() {
+    if (this._view) {
+      this._view.webview.html = this._getHtmlForWebview()
+    }
+  }
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken,
+  ): Thenable<void> | void {
+    this._view = webviewView
+
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    }
+
+    this._updateView()
+  }
+
+  private _getHtmlForWebview() {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+      </head>
+      <body>
+      </body>
+      </html>
+    `
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const provider = new OBSControlViewProvider(context.extensionUri)
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider("obsControl", provider),
+  )
+}
 
 export function deactivate() {}
